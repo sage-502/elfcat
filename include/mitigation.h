b75d6d0e4762dc279f7248047decf3b11793858f
yeon-w <koyeonwoo1104@gmail.com>
2026-04-09 19:17:18 +0900

    Add mitigation structure and analyze function prototype

--- include/mitigation.h
+++ include/mitigation.h
@@ -1,1 +1,17 @@
+#ifndef MITIGATION_H
+#define MITIGATION_H
 
+#include "common.h"
+
+typedef struct s_mitigation
+{
+    int nx;
+    int pie;
+    int relro;
+    int canary;
+
+} mitigation_t;
+
+mitigation_t analyze_mitigation(elf_t *elf);
+
+#endif
