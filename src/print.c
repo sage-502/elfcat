cbfd30549d3fa5a3eb4e2bf838e7eaf6aa3c5e10
yeon-w <koyeonwoo1104@gmail.com>
2026-04-09 19:28:57 +0900

    Update and rename vuln_print.c to print.c

--- src/print.c
+++ src/print.c
@@ -17,3 +17,18 @@ void print_vuln(vuln_t v)
             printf("[%d] %s\n", i + 1, v.messages[i]);
     }
 }
+
+void print_mitigation(mitigation_t m)
+{
+    printf("NX: %s\n", m.nx ? "Enabled" : "Disabled");
+    printf("PIE: %s\n", m.pie ? "Enabled" : "Disabled");
+
+    if (m.relro == 0)
+        printf("RELRO: No RELRO\n");
+    else if (m.relro == 1)
+        printf("RELRO: Partial RELRO\n");
+    else
+        printf("RELRO: Full RELRO\n");
+
+    printf("Canary: %s\n", m.canary ? "Enabled" : "Disabled");
+}
